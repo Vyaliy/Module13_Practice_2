a61c2343df48116c9f38a1d9bdbf0384413d148d
Vyaliy <Vyaltsevn@mail.ru>
2023-03-30 19:20:49 +0300

    New

--- Module13_Practice_2/Program.cs
+++ Module13_Practice_2/Program.cs
@@ -1,43 +1,66 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Module13_Practice_2
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string GetAllTextFromFile(string path)
         {
-            string path = "C:\\Users\\Никита\\source\\repos\\Module13_Practice_2\\Module13_Practice_2\\bin\\Debug\\net7.0\\Text1.txt";
-            //path = Console.ReadLine();
-            List<string> list;
-            string text;
             using (StreamReader sr = new StreamReader(path))
             {
-                text = sr.ReadToEnd().ToLower();
+                return new string(sr.ReadToEnd().Where(c => !char.IsPunctuation(c)).ToArray()).ToLower();
             }
-            text = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
-
-            list = new List<string>(text.Split(' ', '\n', '\t'));
-            list = new List<string>(list.Where(str => !(string.Empty == str)));
-            HashSet<string> hs = new HashSet<string>(list);
+        }
+        public static List<string> StrToListOfWords(string text)
+        {
+            return new List<string>(text.Split(' ', '\n', '\t'));
+        }
+        public static Dictionary<string, int> CreateDictionary(IList<string> list)
+        {
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            KeyValuePair<string, int> maxInDict;
-            string[] topWords = new string[10];
-            foreach (var item in hs)
+            for (int i = 0; i < list.Count; ++i)
             {
-                dict.Add(item, list.Count(str => str == item));
+                if (!dict.TryAdd(list[i], 1))
+                    dict[list[i]]++;
             }
+            dict.Remove(string.Empty);
+            return dict;
+        }
+        public static string[] GetMaxWordsFromDictionary(IDictionary<string, int> dict)
+        {
+            string[] topWords = new string[10];
             string str = string.Empty;
             for (int i = 0; i < topWords.Length; ++i)
             {
                 topWords[i] = dict.MaxBy(x => x.Value).Key;
                 dict.Remove(topWords[i]);
             }
-            foreach (var word in topWords)
+            return topWords;
+        }
+        public static void DisplayCollection<T>(IEnumerable<T> values)
+        {
+            foreach (var word in values)
                 Console.WriteLine(word);
+            return;
+        }
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Никита\\source\\repos\\Module13_Practice_2\\Module13_Practice_2\\bin\\Debug\\net7.0\\Text1.txt";
+            //path = Console.ReadLine();
+            string text = GetAllTextFromFile(path);
+
+            List<string> list = StrToListOfWords(text);
+
+            Dictionary<string, int> dict = CreateDictionary(list);
+
+            string[] topWords = GetMaxWordsFromDictionary(dict);
+
+            DisplayCollection(topWords);
             Console.ReadKey();
         }
     }
